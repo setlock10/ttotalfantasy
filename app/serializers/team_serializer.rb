f7242816304d3ba9cae4e6c5f0bba089a9b5046f
John J Setlock II <johnsetlock@gmail.com>
2023-07-13 08:51:02 -0500

    fixed logos

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,5 +1,5 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :name, :code, :pot_4_2023, :pot_8_2023, :win_total_2023, :over_juice_2023, :under_juice_2023, :elo_2022, :playoffs_2022, :logo, :logo_large
+  attributes :id, :name, :number, :code, :pot_4_2023, :pot_8_2023, :win_total_2023, :over_juice_2023, :under_juice_2023, :elo_2022, :playoffs_2022, :logo, :logo_large
 
   def pot_8_2023
     object.pot8.pot_8_2023
