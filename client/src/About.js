ccfc233f19f7f7591f282fa0379f90012084cacb
John J Setlock II <johnsetlock@gmail.com>
2023-08-18 15:21:20 -0500

    styling

--- client/src/About.js
+++ client/src/About.js
@@ -10,25 +10,28 @@ function About ({setIsLoading, navigate}){
     return(
         <div style={{'position': "relative","top":"100px"}}>
             <p>Welcome to Team Total Fantasy</p>
-
-            <p>Teams are sorted by their expected season win totals and divided into 4 pots. <p/>
+<ul>
+            <li>Teams are sorted by their expected season win totals and divided into 4 pots. </li>
                 
-            <p>You must draft 2 teams from each pot.</p>
+            <li>You must draft 2 teams from each pot.</li>
 
-            <p>One point is awarded for each regular season win</p>
+            <li>One point is awarded for each regular season win</li>
 
-            <p> 5 points are awarded for making the playoffs</p>
+            <li> 5 points are awarded for making the playoffs</li>
 
-            <p>5 points for each playoff win</p>
+            <li>5 points for each playoff win</li>
 
-            <p>An additional 5 points for winning the superbowl</p>
+            <li>An additional 5 points for winning the superbowl</li>
+
+            <li>Set Your Line-Up Before 1:00pm Eastern Time on September 10, 2023</li>
+            <li>There will be scoring updates throught the season.</li>
             
+</ul>
             <p>Inquiries: setlockLLC@gmail.com</p>
             
             <p>Like this project and want to support it? Venmo: @SetlockProjects</p>
 
-
-            </p>
+            
             <button onClick={onClick}>Start Picking!</button>
         </div>
     )
